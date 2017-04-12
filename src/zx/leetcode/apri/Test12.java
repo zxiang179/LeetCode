3370e6cdfb050a3230e84292cdb81f7df4e8a7fb
zx <zxiang248@gmail.com>
2017-04-12 09:21:14 +0800

    268. Missing Number

---
+++ src/zx/leetcode/apri/Test12.java
@@ -0,0 +1,33 @@
+package zx.leetcode.apri;
+/**
+ * 268. Missing Number
+ * @author Carl_Hugo
+ */
+public class Test12 {
+	
+	/**
+	 * 使用hash表
+	 * @param nums
+	 * @return
+	 */
+    public int missingNumber(int[] nums) {
+        int length = nums.length;
+        int[] map = new int[length+1];
+        int missingNum=0;
+        for(int i=0;i<nums.length;i++){
+        	map[nums[i]]=1;
+        }
+        for(int i=0;i<map.length;i++){
+        	if(map[i]==0){
+        		missingNum = i;
+        	}
+        }
+        return missingNum;
+    }
+	
+	public static void main(String[] args) {
+		Test12 test12 = new Test12();
+		System.out.println(test12.missingNumber(new int[]{0,1,3}));
+	}
+
+}
