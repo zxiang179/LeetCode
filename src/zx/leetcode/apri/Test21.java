6fec106a8b322004a641b56a1263348f5bad7eed
zx <zxiang248@gmail.com>
2017-04-23 23:01:15 +0800

    541. Reverse String II

---
+++ src/zx/leetcode/apri/Test21.java
@@ -0,0 +1,20 @@
+package zx.leetcode.apri;
+/**
+ * 541. Reverse String II
+ * @author Carl_Hugo
+ */
+public class Test21 {
+	
+	/**
+	 * 给一个字符串和一个整数k
+	 * 当字符串长度小于k 将字符串整体逆序
+	 * 当字符串长度大于k 小于2k时，将前k个字符串逆序，后面的字符串保持原样
+	 * @param s
+	 * @param k
+	 * @return
+	 */
+    public String reverseStr(String s, int k) {
+        
+    }
+
+}
