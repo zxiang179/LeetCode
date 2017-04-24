42773cf2837c123bded5826bb0106e4e0423ecc5
zx <zxiang248@gmail.com>
2017-04-24 09:50:59 +0800

    438. Find All Anagrams in a String

---
+++ src/zx/leetcode/apri/Test22.java
@@ -0,0 +1,18 @@
+package zx.leetcode.apri;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 438. Find All Anagrams in a String
+ * @author Carl_Hugo
+ */
+public class Test22 {
+	
+    public List<Integer> findAnagrams(String s, String p) {
+        
+        
+    }
+
+}
