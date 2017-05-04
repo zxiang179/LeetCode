6c305611439b6cb528d1d201b0896b337c611bef
zxiang179 <zxiang248@gmail.com>
2017-05-04 18:58:50 +0800

    367. Valid Perfect Square

---
+++ src/zx/leetcode/may/Test9.java
@@ -0,0 +1,23 @@
+package zx.leetcode.may;
+/**
+ * 367. Valid Perfect Square
+ * @author Carl_Hugo
+ * @date 2017年5月4日
+ */
+public class Test9 {
+	
+	/**
+     * Given a positive integer num, write a function which returns True if num is a perfect square else False.
+	 * @param num
+	 * @return
+	 */
+    public boolean isPerfectSquare(int num) {
+        int i;
+    	for(i=1;i<=num/2;i++){
+        	if(i*i>=num)break;
+        }
+    	if(i*i==num)return true;
+    	return false;
+    }
+
+}
