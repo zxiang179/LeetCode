0d1d16959f5033a680e7b0b51a5f92ecd32b30de
zxiang179 <zxiang248@gmail.com>
2017-05-28 11:23:36 +0800

    338. Counting Bits

---
+++ src/zx/leetcode/may/Test30.java
@@ -0,0 +1,21 @@
+package zx.leetcode.may;
+/**
+ * 338. Counting Bits
+ * @author Carl_Hugo
+ * @date 2017年5月28日
+ */
+public class Test30 {
+	
+    public int[] countBits(int num) {
+        int[] res = new int[num+1];
+        int offset = 1;
+        for(int index=1;index<num+1;index++){
+        	if(offset*2==index){
+        		offset*=2;
+        	}
+        	res[index]=res[index-offset]+1;
+        }
+        return res;
+    }
+
+}
