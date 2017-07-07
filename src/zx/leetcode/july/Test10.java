5595c256c06bbac3d3ad7da80fd906d4adaeeff7
zxiang179 <zxiang248@gmail.com>
2017-07-07 14:35:48 +0800

    609. Find Duplicate File in System

---
+++ src/zx/leetcode/july/Test10.java
@@ -0,0 +1,68 @@
+package zx.leetcode.july;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+/**
+ * 609. Find Duplicate File in System
+ * @author Carl_Hugo
+ * @date 2017年7月7日
+ */
+public class Test10 {
+	
+	/**
+	 * Input:
+     * ["root/a 1.txt(abcd) 2.txt(efgh)", "root/c 3.txt(abcd)", "root/c/d 4.txt(efgh)", "root 4.txt(efgh)"]
+     * Output:  
+     * [["root/a/2.txt","root/c/d/4.txt","root/4.txt"],["root/a/1.txt","root/c/3.txt"]]
+	 * @param paths
+	 * @return
+	 */
+	public List<List<String>> findDuplicate(String[] paths) {
+		List<List<String>> resultList = new ArrayList<List<String>>();
+		//key:content value:directory
+		Map<String,ArrayList<String>> map = new HashMap<String,ArrayList<String>>();
+		for(int i=0;i<paths.length;i++){
+			String temp = paths[i];
+			String[] split = paths[i].split("\\s");
+			//root/a 1.txt(abcd) 2.txt(efgh)
+			String rootPath=split[0];
+			for(int j=1;j<split.length;j++){
+				String file = split[j];
+				int startIndex = file.indexOf("(");
+				int endIndex = file.indexOf(")");
+				String content = file.substring(startIndex+1, endIndex);
+				String endPath = file.substring(0,startIndex);
+				String fullPath = rootPath+"/"+endPath;
+				if(map.containsKey(content)){
+					ArrayList<String> subList = map.get(content);
+					subList.add(fullPath);
+				}else{
+					ArrayList<String> subList = new ArrayList<String>();
+					subList.add(fullPath);
+					map.put(content, subList);
+				}
+			}
+		}
+		Set<Entry<String,ArrayList<String>>> entrySet = map.entrySet();
+		Iterator<Entry<String, ArrayList<String>>> iterator = entrySet.iterator();
+		while(iterator.hasNext()){
+			Entry<String, ArrayList<String>> entry = iterator.next();
+			ArrayList list = entry.getValue();
+			if(list.size()>1){
+				resultList.add(entry.getValue());
+			}		
+		}
+		return resultList;
+    }
+	
+	public static void main(String[] args) {
+		new Test10().findDuplicate(new String[]{"root/a 1.txt(abcd) 2.txt(efgh)", "root/c 3.txt(abcd)", "root/c/d 4.txt(efgh)", "root 4.txt(efgh)"});
+	}
+
+}
--- src/zx/leetcode/july/Test9.java
+++ src/zx/leetcode/july/Test9.java
@@ -5,27 +5,42 @@ package zx.leetcode.july;
  * @date 2017年7月6日
  */
 public class Test9 {
+
 	
-    public int numberOfArithmeticSlices(int[] A) {
+	public int numberOfArithmeticSlices(int[] A) {
+        int cur = 0;
+        int sum =0;
+        for(int i=2;i<A.length;i++){
+        	if(A[i]-A[i-1]==A[i-1]-A[i-2]){
+        		cur+=1;
+        		sum+=cur;
+        	}else{
+        		cur=0;
+        	}
+        }
+        return sum;
+        
+    }
+	
+    public int numberOfArithmeticSlices2(int[] A) {
         int step = 0;
         int count = 0;
         int gap=0;
         int element = 3;
-        step = A[1] - A[0];
         if(A.length<3)return 0;
+        step = A[1] - A[0];
         for(int i=2;i<A.length;i++){
         	if(A[i]-A[i-1]!=step)return 0;
         }
-        
         while(element<=A.length){
         	count+=A.length-element+1;
-        	gap=1;
-        	while(((gap+1)*(element-1)+1)<=A.length){
-            	count +=(A.length-((gap+1)*(element-1)+1)+1);
-            	gap++;
-            }
+//        	gap=1;
+//        	while(((gap+1)*(element-1)+1)<=A.length){
+//            	count +=(A.length-((gap+1)*(element-1)+1)+1);
+//            	gap++;
+//            }
+        	element++;
         }
-        
         return count;
     }
 
