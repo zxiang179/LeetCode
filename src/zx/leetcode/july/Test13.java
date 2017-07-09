2ed23a23d9dca94ad506af04d5d862a970531a30
zxiang179 <zxiang248@gmail.com>
2017-07-09 22:04:49 +0800

    F:\javase_practice\LeetCode

--- src/zx/leetcode/july/Test13.java
+++ src/zx/leetcode/july/Test13.java
@@ -6,6 +6,29 @@ package zx.leetcode.july;
  */
 public class Test13 {
 	
-	
+	/**
+	 * 
+	 * @param nums
+	 * @param S
+	 * @return
+	 */
+    public int findTargetSumWays(int[] nums, int S) {
+        int sum=0;
+        for(int n:nums){
+        	sum+=n;
+        }
+        return sum<S||(S+sum)%2>0?0:subsetSum(nums,(S+sum)>>>1);
+    }
+    
+    public int subsetSum(int[] nums,int s){
+    	int dp[] = new int[s+1];
+    	dp[0]=1;
+    	for(int n:nums){
+    		for(int i=s;i>=n;i--){
+    			dp[i]+=dp[i-n];
+    		}
+    	}
+    	return dp[s];
+    }
 
 }
---
+++ src/zx/leetcode/july/Test14.java
@@ -0,0 +1,38 @@
+package zx.leetcode.july;
+/**
+ * 547. Friend Circles
+ * @author Carl_Hugo
+ * @date 2017年7月9日
+ */
+public class Test14 {
+	
+    /**
+     * find friend circles(dfs)
+     * @param M
+     * @return
+     */
+	public int findCircleNum(int[][] M) {
+		int[] visited = new int[M.length];
+		int count = 0;
+		for(int i=0;i<M.length;i++){
+			if(visited[i]==0){
+				dfs(M,visited,i);
+				count++;
+			}
+		}
+        return count;
+    }
+	
+	public void dfs(int[][] M,int[] visited,int i){
+		for(int j=0;j<M.length;j++){
+        	if(M[i][j]==1&&visited[j]==0){
+        		visited[j]=1;
+        		dfs(M,visited,j);
+        	}
+        }
+	}
+
+	public static void main(String[] args) {
+		new Test14().findCircleNum(new int[][]{{1,1,0},{1,1,0},{0,0,1}});
+	}
+}
