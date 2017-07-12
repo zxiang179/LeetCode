ed4ef30aec6add4b6034fe6a3c5c0c1a30ab645c
timlackhan <727781157@qq.com>
2017-07-12 09:16:33 +0800

    524. Longest Word in Dictionary through Deleting

--- src/zx/leetcode/july/Test16.java
+++ src/zx/leetcode/july/Test16.java
@@ -11,6 +11,20 @@ import java.util.List;
  */
 public class Test16 {
 	
+	
+	public String findLongestWord2(String s,List<String> d){
+		String longest = "";
+	    for (String dictWord : d) {
+	        int i = 0;
+	        for (char c : s.toCharArray()) 
+	            if (i < dictWord.length() && c == dictWord.charAt(i)) i++;
+
+	        if (i == dictWord.length() && dictWord.length() >= longest.length()) 
+	            if (dictWord.length() > longest.length() || dictWord.compareTo(longest) < 0)
+	                longest = dictWord;
+	    }
+	    return longest;
+	}
     
 	public String findLongestWord(String s, List<String> d) {
 		int[] hash = new int[26];
@@ -28,23 +42,31 @@ public class Test16 {
 				tempHash[str.charAt(j)-'a']--;
 				if(tempHash[str.charAt(j)-'a']<0){
 					satisfied=false;
+					break;
 				}
 			}
 			if(satisfied==true){
 				satisfiedStr[count++]=str;					
 			}
+			satisfied=true;
 		}
         if(count==0)return "";
-        Arrays.sort(satisfiedStr);
-        return satisfiedStr[0];
+        String longest =satisfiedStr[0];
+        for(int i=1;i<satisfiedStr.length&&satisfiedStr[i]!=null;i++){
+        	if(longest.length()<=satisfiedStr[i].length()){
+        		if(longest.length()<satisfiedStr[i].length()||satisfiedStr[i].compareTo(longest)<0)
+        		longest=satisfiedStr[i];
+        	}
+        }
+        return longest;
     }
 	
 	public static void main(String[] args) {
 		//"abpcplea"
 		//["ale","apple","monkey","plea"]
-		String[] str = {"ale","apple","monkey","plea"};
+		String[] str = {"aaa","aa","a"};
 		List<String> list = Arrays.asList(str);
-		new Test16().findLongestWord("abpcplea", list);
+		new Test16().findLongestWord("aaa", list);
 	}
 
 }
