01622fa9e3c4b6aa3d7ba777dc6e8258cf76e647
timlackhan <727781157@qq.com>
2017-07-24 18:31:46 +0800

    643. Maximum Average Subarray I

--- src/zx/leetcode/july/Test29.java
+++ src/zx/leetcode/july/Test29.java
@@ -1,4 +1,8 @@
 package zx.leetcode.july;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 525. Contiguous Array
  * @author Carl_Hugo
@@ -6,8 +10,28 @@ package zx.leetcode.july;
  */
 public class Test29 {
 	
+	/**
+	 * Given a binary array, 
+	 * find the maximum length of a contiguous subarray with equal number of 0 and 1.
+	 * @param nums
+	 * @return
+	 */
     public int findMaxLength(int[] nums) {
-        
+    	for(int i=0;i<nums.length;i++){
+    		if(nums[i]==0)nums[i]=-1;
+    	}
+        Map<Integer, Integer> sum2Index = new HashMap<Integer,Integer>();
+        sum2Index.put(0, -1);
+        int sum = 0, max = 0;
+        for(int i=0;i<nums.length;i++){
+        	sum+=nums[i];
+        	if(sum2Index.containsKey(sum)){
+        		max = Math.max(max, i-sum2Index.get(sum));
+        	}else{
+        		sum2Index.put(sum, i);
+        	}
+        }
+        return max;
     }
 
 }
