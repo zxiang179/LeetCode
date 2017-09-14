8ac50637959ccac19dbe1c84401ddc6c2c5237dc
timlackhan <zxiang248@gmail.com>
2017-09-14 10:48:16 +0800

    216. Combination Sum III

---
+++ src/zx/leetcode/Sep/Combination_Sum_III.java
@@ -0,0 +1,34 @@
+package zx.leetcode.Sep;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 216. Combination Sum III
+ * @author Carl_Hugo
+ * 2017年9月14日 上午9:26:28
+ */
+public class Combination_Sum_III {
+	
+	public List<List<Integer>> combinationSum3(int k, int n) {
+		List<List<Integer>> ans = new ArrayList<>();
+		combination(ans,new ArrayList<Integer>(),k,1,n);
+		return ans;
+    }
+	
+	private void combination(List<List<Integer>> ans,
+			List<Integer> comb,int k,int start,int n){
+		if(comb.size()==k&&n==0){
+			List<Integer> li = new ArrayList<Integer>(comb);
+			ans.add(li);
+			return;
+		}
+		for(int i=start;i<=9;i++){
+			comb.add(i);
+			combination(ans,comb,k,i+1,n-i);
+			comb.remove(comb.size()-1);
+		}
+		
+	}
+
+}
