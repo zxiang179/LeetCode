7fee534521f8886bb3e70c64d5bdf4895414266a
zxiang179 <zxiang248@gmail.com>
2017-10-14 17:44:03 +0800

    Two Sum

---
+++ src/zx/leetcode/restart/Two_Sum.java
@@ -0,0 +1,22 @@
+package zx.leetcode.restart;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Two_Sum {
+	
+	public int[] twoSum(int[] nums, int target) {
+		Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+		int[] res = new int[2];
+		for(int i=0;i<nums.length;i++){
+			if(!map.containsKey(target-nums[i])){
+				map.put(nums[i], i);
+			}else{
+				res[0]=map.get(target-nums[i]);
+				res[1]=i;
+			}
+		}
+        return res;
+    }
+
+}
