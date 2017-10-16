a3240e1b2026b37dc839677cb15b99f2e3f5624a
timlackhan <zxiang248@gmail.com>
2017-10-16 19:02:37 +0800

    thirdMax

---
+++ src/zx/leetcode/restart/Third_Maximum_Number.java
@@ -0,0 +1,34 @@
+package zx.leetcode.restart;
+
+import org.junit.Test;
+
+public class Third_Maximum_Number {
+	
+	public int thirdMax(int[] nums) {
+		Integer first = null;
+		Integer second = null;
+		Integer third = null;
+
+		for(int i=0;i<nums.length;i++){
+			if(first==null||nums[i]>first){
+				third = second;
+				second = first;
+				first = nums[i];
+			}else if(second==null||nums[i]>second){
+				if(nums[i]==first)continue;
+				third = second;
+				second = nums[i];
+			}else if(third==null||nums[i]>third){
+				if(nums[i]==second)continue;
+				third = nums[i];
+			}
+		}
+        return third==null?first:third;				
+	}
+	
+	@Test
+	public void test(){
+		thirdMax(new int[]{2,3,2,1});
+	}
+
+}
