1e3237d1845f5636ed150ba3fd5d908865edbb1c
timlackhan <zxiang248@gmail.com>
2017-10-17 20:30:17 +0800

    Contains_Duplicate_II

---
+++ src/zx/leetcode/restart/Contains_Duplicate_II.java
@@ -0,0 +1,30 @@
+package zx.leetcode.restart;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Contains_Duplicate_II {
+	
+	public boolean containsNearbyDuplicate(int[] nums,int k){
+		if(nums==null||nums.length==0||k<0)return false;
+		Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+		//flag 表示存在
+		boolean flag = false;
+		for(int i=0;i<nums.length;i++){
+			if(!map.containsKey(nums[i])){
+				map.put(nums[i], i);
+			}else{
+				int j = map.get(nums[i]);
+				if(i-j<=k){
+					flag = true;
+					break;
+				}else{
+					map.put(nums[i], i);
+				}
+			}
+		}
+		return flag;
+		
+	}
+
+}
