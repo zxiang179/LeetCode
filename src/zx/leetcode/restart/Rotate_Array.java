511a503870ea7ff963ff2249c61563d8b2b59f2a
timlackhan <zxiang248@gmail.com>
2017-10-18 15:40:11 +0800

    rotate

---
+++ src/zx/leetcode/restart/Rotate_Array.java
@@ -0,0 +1,22 @@
+package zx.leetcode.restart;
+
+public class Rotate_Array {
+	
+	public void rotate(int[] nums,int k){
+		int len = nums.length;
+		k=k%len;
+		reverse(nums,0,len);
+		reverse(nums,0,k-1);
+		reverse(nums,k,len-1);
+	}
+	
+	public static void reverse(int[] nums,int start,int end){
+		int temp;
+		for(int i=start,j=end;i<=j;i++,j--){
+			temp = nums[i];
+			nums[i] = nums[j];
+			nums[j] = temp;
+		}
+	}
+
+}
