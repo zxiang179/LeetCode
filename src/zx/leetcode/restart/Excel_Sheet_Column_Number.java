075d574f9b1ae0be61cec95a3521c712ae108d2a
timlackhan <zxiang248@gmail.com>
2017-10-18 19:20:21 +0800

    Excel_Sheet_Column_Number

---
+++ src/zx/leetcode/restart/Excel_Sheet_Column_Number.java
@@ -0,0 +1,24 @@
+package zx.leetcode.restart;
+
+import org.junit.Test;
+
+public class Excel_Sheet_Column_Number {
+	
+	 public int titleToNumber(String s) {
+		 int result = 0;
+		 char[] cArr = s.toCharArray();
+		 int count = 0;
+		 for(int i=cArr.length-1;i>=0;i--){
+			 int temp = cArr[i]-'A'+1;
+			 result += temp*Math.pow(26, count);
+			 count++;
+		 }
+		 return result;
+	 }
+	 
+	 @Test
+	 public void test(){
+		 System.out.println(titleToNumber("Z"));
+	 }
+
+}
