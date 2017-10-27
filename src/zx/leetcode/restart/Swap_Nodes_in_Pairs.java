ec8f96dda1e4b531c2d3c4f0f47a4d755b22041e
timlackhan <zxiang248@gmail.com>
2017-10-27 13:35:01 +0800

    Swap_Nodes_in_Pairs

---
+++ src/zx/leetcode/restart/Swap_Nodes_in_Pairs.java
@@ -0,0 +1,36 @@
+package zx.leetcode.restart;
+
+import zx.leetcode.restart.Remove_Linked_List_Elements.ListNode;
+
+public class Swap_Nodes_in_Pairs {
+	
+	public ListNode swapPairs(ListNode head) {
+		if(head==null||head.next==null){
+			return head;
+		}
+		ListNode temp = head.next;
+		head.next = swapPairs(head.next.next);
+		temp.next = head;
+        return temp;
+    }
+	
+	public ListNode swapPairs2(ListNode head) {
+		if(head==null||head.next==null){
+			return head;
+		}
+		ListNode dummyNode = new ListNode(0);
+		dummyNode.next = head;
+		ListNode current = dummyNode;
+		while(current.next!=null&&current.next.next!=null){
+			ListNode first = current.next;
+			ListNode second = current.next.next;
+			first.next = second.next;
+			second.next = first;
+			current.next = second;
+			current = current.next.next;
+		}
+		return dummyNode.next;
+
+    }
+
+}
