24d7f061abdfa0dd032f5cde0e4d6e190444832b
timlackhan <zxiang248@gmail.com>
2018-03-05 10:38:14 +0800

    Maximal_Square_221

--- src/zx/leetcode/dog/mar/Maximal_Square_221.java
+++ src/zx/leetcode/dog/mar/Maximal_Square_221.java
@@ -16,6 +16,7 @@ public class Maximal_Square_221 {
 	 * @return
 	 */
     public int maximalSquare(char[][] matrix) {
+    	if(matrix==null||matrix.length==0)return 0;
     	int m = matrix.length;
     	int n = matrix[0].length;
     	int[][] dp = new int[m][n];
@@ -29,6 +30,36 @@ public class Maximal_Square_221 {
         				dp[i][j] = Math.min(dp[i-1][j-1],Math.min(dp[i-1][j],dp[i][j-1]))+1;
         			}	
     			}
+    			if(res<dp[i][j])res = dp[i][j];
+    		}
+    	}
+	    return res*res;
+	}
+    
+    public int maximalSquare2(char[][] matrix) {
+    	if(matrix==null||matrix.length==0)return 0;
+    	int m = matrix.length;
+    	int n = matrix[0].length;
+    	int[][] dp = new int[m][n];
+    	int res = 0;
+    	for(int i=0;i<m;i++){
+    		if(matrix[i][0]=='1'){
+    			dp[i][0] = 1;
+    			res = 1;
+    		}
+    	}
+    	for(int j=0;j<n;j++){
+    		if(matrix[0][j]=='1'){
+    			dp[0][j] = 1;
+    			res = 1;
+    		}
+    	}
+    	
+    	for(int i=1;i<m;i++){
+    		for(int j=1;j<n;j++){
+				if(matrix[i][j]=='1'){
+    				dp[i][j] = Math.min(dp[i-1][j-1],Math.min(dp[i-1][j],dp[i][j-1]))+1;
+    			}	
     			if(res<dp[i][j])res = dp[i][j];
     		}
     	}
