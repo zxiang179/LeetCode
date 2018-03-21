d7d709058e7e5d7928e9cbdfd7de004f6da6ccf7
timlackhan <zxiang248@gmail.com>
2018-03-21 22:42:11 +0800

    Find_Duplicate_Subtrees_652

--- src/zx/leetcode/dog/mar/Find_Duplicate_Subtrees_652.java
+++ src/zx/leetcode/dog/mar/Find_Duplicate_Subtrees_652.java
@@ -24,21 +24,34 @@ public class Find_Duplicate_Subtrees_652 {
 	
 	private String serializeSubTree(TreeNode node,Map<String,List<TreeNode>> map){
 		if(node==null)return "";
+		//得到所有子树
 		String s = "<"+serializeSubTree(node.left,map)+node.val+serializeSubTree(node.right,map)+">";
+		System.out.println(s);
 		if(!map.containsKey(s)){
 			map.put(s, new ArrayList<>());
 		}
+		//将子树中所有的节点添加到value中
 		map.get(s).add(node);
 		return s;
 	}
 	
-	class TreeNode{
+	static class TreeNode{
 		int val;
 		TreeNode left;
 		TreeNode right;
 		public TreeNode(int x){
 			val = x;
 		}
+	}
+	
+	public static void main(String[] args) {
+		TreeNode root = new TreeNode(2);
+		root.left = new TreeNode(1);
+		root.left.left = new TreeNode(3);
+		root.left.right = new TreeNode(4);
+		root.right = new TreeNode(1);
+		root.right.left = new TreeNode(3);
+		new Find_Duplicate_Subtrees_652().findDuplicateSubtrees(root);
 	}
 
 }
